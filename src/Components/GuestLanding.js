985e4c4311f5521acef1fbb80e5ef3fd3221b560
Trey Waller <Trey@Treys-MacBook-Pro.local>
2020-08-28 15:08:46 -0500

    backend authorizes spotify account then redirects to react app

--- src/Components/GuestLanding.js
+++ src/Components/GuestLanding.js
@@ -1,19 +1,9 @@
 import React from "react";
-import Axios from "axios";
 
 export default function Guestlanding() {
-    const scopes = 'user-read-private user-read-email';
-
     return (
         <div className='guest-landing'>
-            <a href={
-            'https://accounts.spotify.com/authorize' +
-            '?response_type=code' +
-            '&client_id=' + "73024cba67cd4fa9b00ce451a3accb1f" +
-            (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-            '&redirect_uri=' + encodeURIComponent('http://localhost:3000/#/home')}>
-                <button></button>
-            </a>
+            Hello!
         </div>
     )
 }
