0c1bce4d6f854764e2eaa35cc05a6d6641f4dfb9
Trey <tlwaller99@gmail.com>
2020-10-12 21:13:04 -0500

    album list styling

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Albumlist(props) {
     const [albums, setAlbums] = useState(props.albums);
 
     return (
-        <div>
-            <ul>
-                {
-                    console.log(albums),
-                    albums.map((e, i) => {
-                        return (
-                        <li key={i} className="album">
-                            <img src={e.album.images[1].url} width="150"/>
-                            <h1>{e.album.name}</h1>
-                        </li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
+        <ol id="album-list-container">
+            <dl>
+                <dt className="dt-title">Title</dt>
+                <dt className="dt-artist">Artist</dt>
+                <dt className="dt-date">Date Liked</dt>
+            </dl>
+            {
+                albums.map((e, i) => {
+                    return (
+                    <li key={i} className="album">
+                        <Link to="/#" className="album-link-wrapper">
+                            <img alt="album-cover" src={e.album.images[2].url} className="album-cover" width="64" height="64"/>
+                            <h1 className="album-title">{e.album.name}</h1>
+                        </Link>
+                    </li>
+                    )
+                })
+            }
+        </ol>
     )
 }
